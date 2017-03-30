83f52ac7d028e08f323bc61d82baa8c8f5505eb5
xoviat <xoviat@users.noreply.github.com>
2017-03-30 13:37:48 -0500

    check the path.

--- go_msi/__init__.py
+++ go_msi/__init__.py
@@ -7,6 +7,26 @@ def program_files_list():
     return [os.environ["ProgramFiles"], os.environ["ProgramFiles(x86)"]]
 
 
+def which(program):
+    import os
+
+    def is_exe(fpath):
+        return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ["PATH"].split(os.pathsep):
+            path = path.strip('"')
+            exe_file = os.path.join(path, program)
+            if is_exe(exe_file):
+                return exe_file
+
+    return None
+
+
 @functools.lru_cache(maxsize=128)
 def find_wix_toolset():
     path = ''
@@ -25,6 +45,10 @@ def find_wix_toolset():
         except IndexError:
             pass
 
+    # if not found, check the path
+    if not os.path.isfile(path):
+        path = which('candle.exe')
+
     if not os.path.isfile(path):
         raise RuntimeError('cannot find wix toolset')
 
@@ -39,6 +63,10 @@ def find_go_msi():
 
         if os.path.isfile(path):
             break
+
+    # if not found, check the path
+    if not os.path.isfile(path):
+        path = which('go-msi.exe')
 
     if not os.path.isfile(path):
         raise RuntimeError('cannot find go-msi')
